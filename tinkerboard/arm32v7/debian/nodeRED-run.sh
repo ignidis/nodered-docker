3c9c60f5f8e21f59e3529c0e8ebf20b2080b44dd
ignidis <patch@cybearth.net>
2018-11-21 17:11:30 +0100

    corrections to run with non root host service
    No need for sudo as initial volume access is granted during initial host preparation
    Owner change of mounted volume files from inside the container is not allowed/needed in the given scenario
    If required the Host must allow it

--- tinkerboard/arm32v7/debian/nodeRED-run.sh
+++ tinkerboard/arm32v7/debian/nodeRED-run.sh
@@ -2,16 +2,11 @@
 #
 # Echo DEBUG Info to the console
 #
-USER="$(whoami)"
-USER_ID="$(id -u ${USER})"
-echo "Service Account: $USER $USER_ID"
-#
-# Set Permissions for home and persistent storage volumes
-#
-sudo chown -R "$USER":"$USER" /home/"$USER"/;
-sudo chown -R "$USER":root /home/"$USER"/.node-red/;
-cd /home/"$USER";
+USER="$(id -un)"
+USER_ID="$(id -u)"
+echo "Service Account: $USER $USER_ID";
 #
 # Start node-red
 #
-node-red
+cd /home/"$USER";
+node-red;
