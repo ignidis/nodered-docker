c18d1225c04494f6ac840ca50c4c674e686e2f69
ignidis <patch@cybearth.net>
2020-11-02 21:06:53 +0100

    current version

--- tinkerboard/arm32v7/debian/build.sh
+++ tinkerboard/arm32v7/debian/build.sh
@@ -2,8 +2,11 @@
 #
 # docker build nodered image
 # Usage:
-#        [sudo] build.sh <nodered-version> <node-lts-version>
+#        [sudo] build.sh <nodered-version> <node-lts-version> <registry> <registry-user> <registry-pwd> <project>
 #
 # Must run as superuser, either you are root or must sudo 
 #
-docker build --build-arg NODERED_VERSION="$1" --build-arg NODE_VERSION="$2" --build-arg NODE_SVC_NAME="node" --build-arg NODE_SVC_UID="9001" -t nodered:"$1"-arm32v7-debian .
+docker build --build-arg NODERED_VERSION="$1" --build-arg NODE_VERSION="$2" --build-arg NODE_SVC_NAME="node" --build-arg NODE_SVC_UID="9001" -t nodered:"$1"-arm32v7-debian . && \
+docker login -p "$5" -u "$4" "$3" && \
+docker tag nodered:"$1"-arm32v7-debian "$3"/"$6"/nodered:"$1"-arm32v7-debian && \
+docker push "$3"/"$6"/nodered:"$1"-arm32v7-debian
